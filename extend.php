dc93a72dc55913e6f807c3c65ed26afec417e4aa
shangyou <wangshangyou@inmyshow.com>
2021-12-28 16:06:59 +0800

    优化

---
+++ extend.php
@@ -0,0 +1,11 @@
+<?php
+
+use Flarum\Extend;
+use Flarum\Frontend\Document;
+
+return [
+    (new Extend\Frontend('forum'))
+        ->content(function (Document $document) {
+            $document->head[] = '<script>alert("你好，世界！")</script>';
+        })
+];
